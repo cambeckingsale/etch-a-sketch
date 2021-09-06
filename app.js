ce22b0d8f920902aff1209de4d26921af0f459a7
cambeckingsale <cambeckingsale@gmail.com>
2021-09-07 00:26:56 +1000

    cant get wipe to work'

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
-let defaultNumPixelsWide = 50;
+let defaultNumPixelsWide = 22;
 let paintColour = 'black'
 let randomFlag = 0;
 
 const paintPixel = (e) => {
-    if (randomFlag == 1) {
-        paintColour = '#' + Math.floor(Math.random()*0xFFFFFF).toString(16);
-        console.log(paintColour);
+    if (!e.shiftKey) {
+        if (randomFlag == 1) {
+            paintColour = '#' + Math.floor(Math.random()*0xFFFFFF).toString(16);
+            document.querySelector('#colour-selector').value = paintColour;
+
+        }
+        e.target.style.backgroundColor = paintColour;
     }
-    e.target.style.backgroundColor = paintColour;
     
+}
+
+const clearAnimation = (numPixelsWide, numPixelsHigh) => {
+    const sketchPad = document.querySelector(".sketch-pad");
+    const pixels = document.querySelectorAll('.pixel')
+    pixels.forEach((pixel) => {
+        setTimeout( () => {
+            pixel.style.backgroundColor = 'black'
+            console.log(pixel)
+        }, 1000) 
+    })
+    while (sketchPad.firstChild) {
+        sketchPad.firstChild.remove()
+    }
 }
 
 const setupSketchPad = (numPixelsWide) => {
     const sketchPad = document.querySelector(".sketch-pad");
-    while (sketchPad.firstChild) {
-        sketchPad.firstChild.remove()
-    }
     let width = sketchPad.offsetWidth
     let height = sketchPad.offsetHeight
-    let numPixelsHigh = height / (width/numPixelsWide);
+    let numPixelsHigh = Math.ceil(height / (width/numPixelsWide));
+    clearAnimation(numPixelsWide, numPixelsHigh)
     sketchPad.style.setProperty('--cols', numPixelsWide);
     sketchPad.style.setProperty('--rows', numPixelsHigh);
-    for (let i = 0; i < (numPixelsWide*Math.ceil(numPixelsHigh)); i++) {
+    for (let i = 0; i < (numPixelsWide*numPixelsHigh); i++) {
         let pixel = document.createElement("div");
         pixel.classList.add("pixel");
         pixel.addEventListener('mousemove', paintPixel);
@@ -35,7 +50,7 @@ const setUpControls = () => {
     const randomColourButton = document.querySelector('#random-colour-button');
     const colourSelector = document.querySelector('#colour-selector');
     colourSelector.value = '#000000';
-    pixelSlider.value = '50';
+    pixelSlider.value = defaultNumPixelsWide;
     pixelSlider.addEventListener('change', (e) => {
         setupSketchPad(e.target.value);
     });
